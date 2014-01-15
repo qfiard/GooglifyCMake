b8375e9dc23ea8f0152b2a5e2c6499787071867c
Quentin Fiard <quentin.fiard@polytechnique.org>
2014-01-15 09:56:51 +0100

    Added cc_test example

---
+++ src/examples/a_package/my_lib_test.cc
@@ -0,0 +1,33 @@
+#include "examples/a_package/my_lib.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace examples {
+namespace a_package {
+namespace {
+
+TEST(MyLibTest, DummyTest) {
+  MyLib::HelloWorld();
+}
+
+class MyMock {
+ public:
+  MOCK_METHOD0(MyMethod, void());
+};
+
+TEST(MyLibTest, DummyTestWithMock) {
+  MyMock mock;
+  EXPECT_CALL(mock, MyMethod());
+  mock.MyMethod();
+}
+
+
+
+}  // namespace
+}  // namespace a_package
+}  // namespace examples
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
+}
