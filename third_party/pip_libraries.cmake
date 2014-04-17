9b6e554af8f7f2dff3bbd426fe77b33b0eed2b8f
Quentin Fiard <quentin.fiard@polytechnique.org>
2014-04-17 09:28:15 +0200

    Added support for more pip libraries

--- third_party/pip_libraries.cmake
+++ third_party/pip_libraries.cmake
@@ -4,7 +4,7 @@ if (DEFINED THIRD_PARTY_PIP_LIBRARIES_CMAKE_)
 endif ()
 set(THIRD_PARTY_PIP_LIBRARIES_CMAKE_ TRUE)
 
-function(pip_library LIB)
+function(pip_library LIB REF)
   set(FULL_NAME third_party.pip.${LIB})
   set(${FULL_NAME} "dummy" PARENT_SCOPE)  # No link occurs in python case.
   set(TARGET ${FULL_NAME}_target)
@@ -13,22 +13,25 @@ function(pip_library LIB)
   add_dependencies(${TARGET} ${VIRTUALENV_TARGET})
   add_custom_command(
     TARGET ${TARGET} POST_BUILD
-    COMMAND ${ARGN} /usr/bin/env pip install "${LIB}" >/dev/null
+    COMMAND ${ARGN} /usr/bin/env pip install "${REF}" >/dev/null
     DEPENDS ${VIRTUALENV_TARGET})
 endfunction(pip_library)
 
 pip_library(
-    Pillow CC=/usr/bin/gcc
+    Pillow Pillow CC=/usr/bin/gcc
     ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future)
-pip_library(PyTrie)
-pip_library(beautifulsoup4)
-pip_library(cssselect)
-pip_library(django)
-pip_library(marisa-trie CC=/usr/bin/gcc CXX=/usr/bin/g++)
-pip_library(numpy CC=/usr/bin/gcc)
-pip_library(psd-tools)
-pip_library(pycountry)
-pip_library(python-gflags)
-pip_library(requests)
-pip_library(scikit-learn CC=/usr/bin/gcc CXX=/usr/bin/g++)
-pip_library(scipy CC=/usr/bin/gcc CXX=/usr/bin/g++)
+pip_library(PyTrie PyTrie)
+pip_library(beautifulsoup4 beautifulsoup4)
+pip_library(cssselect cssselect)
+pip_library(django django)
+pip_library(django-nonrel git+git://github.com/django-nonrel/django)
+pip_library(djangotoolbox git+git://github.com/django-nonrel/djangotoolbox)
+pip_library(marisa-trie marisa-trie CC=/usr/bin/gcc CXX=/usr/bin/g++)
+pip_library(mongodb-engine git+git://github.com/django-nonrel/mongodb-engine)
+pip_library(numpy numpy CC=/usr/bin/gcc)
+pip_library(psd-tools psd-tools)
+pip_library(pycountry pycountry)
+pip_library(python-gflags python-gflags)
+pip_library(requests requests)
+pip_library(scikit-learn scikit-learn CC=/usr/bin/gcc CXX=/usr/bin/g++)
+pip_library(scipy scipy CC=/usr/bin/gcc CXX=/usr/bin/g++)
